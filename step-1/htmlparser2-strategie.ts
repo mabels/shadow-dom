67b9d71728ee35a286e09311b184554f93515824
Meno Abels <meno.abels@adviser.com>
2017-12-14 19:48:38 +0100

    * basic htmlparser test

--- step-1/htmlparser2-strategie.ts
+++ step-1/htmlparser2-strategie.ts
@@ -1,20 +1,43 @@
 import * as htmlparser from 'htmlparser2';
 // import DomHandler from 'domHandler';
 import * as RxMe from 'rxme';
-const DomHandler = require('domhandler');
+const DomHandler = require('domhandler'); // not nice but working
 
-export function parse(inp: RxMe.Subject<string>, out: RxMe.Subject<Node>): void {
-  const dhandler = new DomHandler((_: any, dom: any) => {
-    console.log(dom);
+function _walk(dom: any, obs: RxMe.Observer<any>): void {
+  dom.forEach((element: any) => {
+    obs.next(RxMe.data(element));
+    if (element.children) {
+      _walk(element.children, obs);
+    }
   });
-  const parser = new htmlparser.Parser(dhandler);
-  inp.matchComplete((_, complete) => {
-    parser.end();
-    return true;
-  }).matchType<string>(RxMe.Match.STRING, (_, htmlfrag) => {
-    parser.write(htmlfrag);
-    return true;
-  }).passTo(out);
+}
+
+export function walk(dom: any): RxMe.Observable<any> {
+  return RxMe.Observable.create<any>(null, (obs) => {
+    _walk(dom, obs);
+    obs.complete();
+  });
+}
+
+export function parse(inp: RxMe.Observable<string>): RxMe.Observable<any> {
+  return RxMe.Observable.create<any>(null, (obs) => {
+    let completeSubject: RxMe.Subject<any>;
+    const dhandler = new DomHandler((_: any, dom: any) => {
+      completeSubject.next(RxMe.data(dom));
+      completeSubject.next(RxMe.complete());
+      completeSubject.complete();
+    });
+
+    const parser = new htmlparser.Parser(dhandler);
+    inp.matchComplete((subj, complete) => {
+      completeSubject = subj;
+      parser.end();
+      return subj;
+    }).matchType<string>(RxMe.Match.STRING, (_, htmlfrag) => {
+      parser.write(htmlfrag);
+      return true;
+    }).passTo(obs);
+  });
   // const handler = new DomHandler();
 }
 
--- test/step-1/css-to-ast-test.ts
+++ test/step-1/css-to-ast-test.ts
@@ -3,6 +3,8 @@ import * as RxMe from 'rxme';
 import { Result, Rule, Declaration } from 'postcss';
 import * as SelectorTree from '../../step-1/selector-tree';
 import { assert } from 'chai';
+import * as HtmlParser from '../../step-1/htmlparser2-strategie';
+import { } from 'htmlparser2';
 
 function selectors(): string[] {
   return [
@@ -14,6 +16,21 @@ function selectors(): string[] {
     '#id.classname .classname#id',
     '*, *::after, *::before'
   ];
+}
+
+function html(): string {
+  return `
+    <html>
+    <head>
+      <title>test</title>
+    </head>
+    <body>
+      <h1>Hello World</h1>
+      <p>
+        crazy World
+      </p>
+    </body>
+  `;
 }
 
 function testCss(): string {
@@ -52,31 +69,52 @@ describe('css-to-ast', () => {
 
   it('buildSelectorTree', async () => {
     return new Promise((rs, rj) => {
-    const astCss = cssToAst.fromString(testCss());
-    let count = 0;
-    let hs: SelectorTree.SelectorNode[] = [];
-    SelectorTree.build(astCss).match((_, st) => {
-      st.root.walk().match((__, sn) => {
-        if (sn.selectorParts()[0][0].part.match(/^h[123456]$/) &&
+      const astCss = cssToAst.fromString(testCss());
+      let count = 0;
+      let hs: SelectorTree.SelectorNode[] = [];
+      SelectorTree.build(astCss).match((_, st) => {
+        st.root.walk().match((__, sn) => {
+          if (sn.selectorParts()[0][0].part.match(/^h[123456]$/) &&
             sn.selectorParts()[0][0].selectorType == SelectorTree.SelectorType.TAG) {
-          hs.push(sn);
-        }
-        count++;
-        return true;
-      }).matchComplete((___, cn) => {
-        try {
-          const fs = hs.map(sn => sn.rule.nodes.find((n: any) => n.prop == 'font-size') as Declaration);
-          assert.equal(hs.length, 6, 'hs length');
-          assert.equal(hs.length, fs.filter(f => f.value == fs[0].value).length, 'fs.length');
-          assert.equal(27, count);
-          rs();
-        } catch (e) {
-          rj(e);
-        }
+            hs.push(sn);
+          }
+          count++;
+          return true;
+        }).matchComplete((___, cn) => {
+          try {
+            const fs = hs.map(sn => sn.rule.nodes.find((n: any) => n.prop == 'font-size') as Declaration);
+            assert.equal(hs.length, 6, 'hs length');
+            assert.equal(hs.length, fs.filter(f => f.value == fs[0].value).length, 'fs.length');
+            assert.equal(27, count);
+            rs();
+          } catch (e) {
+            rj(e);
+          }
+          return true;
+        }).passTo();
         return true;
       }).passTo();
-      return true;
-    }).passTo();
+    });
+  });
+
+  it('parsehtml', async () => {
+    return new Promise((rs, rj) => {
+      const rxIn = RxMe.Observable.create<string>(RxMe.Match.STRING, (obs) => {
+        obs.next(RxMe.data(html()));
+        obs.complete();
+      });
+      HtmlParser.parse(rxIn).matchComplete((_, ce) => {
+        rs();
+        return true;
+      }).wildCard((_, rx) => {
+        HtmlParser.walk(rx.data).wildCard((__, rxx) => {
+          if (rxx.data.name) {
+            console.log(rxx.data.type, rxx.data.name);
+          }
+          return true;
+        }).passTo();
+        return true;
+      }).passTo();
     });
   });
 
